702bff7d62485f9d5c31cff03e498a283c19e67d
shenw <devilgeorge@163.com>
2018-03-28 11:27:21 +0800

    OAuth

---
+++ IdentityServerSample/IdentityServerCenter/Config.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerCenter
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetResource()
+        {
+            return new List<ApiResource> {
+                new ApiResource("api","My Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClient()
+        {
+            return new List<Client>
+            {
+                new Client(){
+                    ClientId="Client",
+                    AllowedGrantTypes= GrantTypes.ClientCredentials,
+                    ClientSecrets={new Secret("secret".Sha256()) },
+                    AllowedScopes={"api"}
+                }
+            };
+        }
+    }
+}
